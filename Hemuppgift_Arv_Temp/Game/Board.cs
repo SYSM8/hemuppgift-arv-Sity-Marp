e12e00888db05ba5ee8dc558f2ff28ba80f66f8f
Sity-Marp <88293493+Sity-Marp@users.noreply.github.com>
2024-10-12 19:21:50 +0200

    Add Board Class
    Add folder named Game
    Move TakePins to Game
    
    Add Board class
    Add int noPins property to board.
    Add SetUp, getNoPins, TakePins methods to Board class.
    
    Add temp test code to TakePins

---
+++ Hemuppgift_Arv_Temp/Game/Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp.Game
+{
+    internal class Board
+    {
+        private int noPins { get; set; }
+
+
+        public void SetUp(int value)
+        {
+            noPins = value;
+        }
+
+        public int getNoPins()
+        {
+            return noPins;
+        }
+
+        public void TakePins(int pins)
+        {
+            Console.WriteLine($"You take {pins}");
+            noPins -= pins;
+        }
+
+
+    }
+}
---
+++ Hemuppgift_Arv_Temp/Game/TakePins.cs
@@ -0,0 +1,26 @@
+﻿namespace Hemuppgift_Arv_Temp.Game
+{
+    internal class TakePins
+    {
+        //Här är main klassen där koden ska testas, lägg in i mappen
+        static void Main(string[] args)
+        {
+
+            Board GameBoard = new Board();
+
+            Console.WriteLine("How many pins this round?");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int PinNmbr))
+            {
+                GameBoard.SetUp(PinNmbr);
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+
+            GameBoard.TakePins(5);
+            Console.WriteLine(GameBoard.getNoPins());
+        }
+    }
+}
